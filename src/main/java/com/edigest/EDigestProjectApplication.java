6f02cec477409163441694d3f3be1d29e2752824
biswa-ranjan-dev <biswa.rout.profile@gmail.com>
2025-11-15 20:14:03 +0530

    Initial project commit

---
+++ src/main/java/com/edigest/EDigestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.edigest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDigestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EDigestProjectApplication.class, args);
+    }
+
+}
